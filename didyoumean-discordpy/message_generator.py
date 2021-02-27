1ecff4fb77b96c5a4b9af05fc3c61c0851c6fcd0
daima3629 <daima3629@usbc.be>
2021-02-28 01:23:24 +0900

    [Add&Change] メッセージジェネレータにdocstringを追加&引数名等一部変更

--- didyoumean-discordpy/message_generator.py
+++ didyoumean-discordpy/message_generator.py
@@ -4,11 +4,32 @@ from typing import List
 
 
 class MessageGenerator:
-    def __init__(self, command_name: str, similar_cmd_list: List[str]) -> None:
-        self.command_name = command_name
+    """
+    Base class for the message generator.
+    You should inherit from this class to implement your message generator.
+
+    Parameters
+    ----------
+    invalid_command_name : str
+        The invalid command name.
+    similar_cmd_list : List[str]
+        The similar command name list to `invalid_cmd_name`.
+    """
+    def __init__(self, invalid_cmd_name: str, similar_cmd_list: List[str]) -> None:
+        self.invalid_cmd_name = invalid_cmd_name
         self.similar_cmd_list = similar_cmd_list
 
     async def send(self, ctx: commands.Context):
+        """
+        The method to be called when sending a message.
+        You must override this method.
+
+        Parameters
+        ----------
+        ctx : commands.Context
+            The context that should be used to send message.
+
+        """
         raise NotImplementedError
 
 
@@ -16,7 +37,7 @@ class DefaultMessageGenerator(MessageGenerator):
     async def send(self, ctx):
         text = textwrap.dedent(
             f"""\
-            Command `{self.command_name}` not found.
+            Command `{self.invalid_cmd_name}` not found.
             did you mean: """
         )
         wrapped_similar_cmd_list = [f"`{c}`" for c in self.similar_cmd_list]
